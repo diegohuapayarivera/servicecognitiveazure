3ddba1703c482961716d9b9df947fb4b28f79cd0
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2021-07-01 13:08:51 -0500

    :sunglasses:

---
+++ trabajo7/src/main/java/modelo/Pokemon.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author diego
+ */
+public class Pokemon {
+
+    private int IDPOK;
+    private int IDHAB;
+    private String NOMPOK;
+    private String APOPOK;
+    private double EXPHABPOK;
+    private String EXPHABPOKString;
+    
+    private double ALTPOK;
+    private String ALTPOKString;
+    
+    private double PESPOK;
+    private String PESPOKString;
+    
+    private String IMGFROPOK;
+    
+    private String IMGATRPOK;
+    
+    private Habilidades habilidades;
+
+    public int getIDPOK() {
+        return IDPOK;
+    }
+
+    public void setIDPOK(int IDPOK) {
+        this.IDPOK = IDPOK;
+    }
+
+    public int getIDHAB() {
+        return IDHAB;
+    }
+
+    public void setIDHAB(int IDHAB) {
+        this.IDHAB = IDHAB;
+    }
+
+    public String getNOMPOK() {
+        return NOMPOK;
+    }
+
+    public void setNOMPOK(String NOMPOK) {
+        this.NOMPOK = NOMPOK;
+    }
+
+    public String getAPOPOK() {
+        return APOPOK;
+    }
+
+    public void setAPOPOK(String APOPOK) {
+        this.APOPOK = APOPOK;
+    }
+
+    public double getEXPHABPOK() {
+        return EXPHABPOK;
+    }
+
+    public void setEXPHABPOK(double EXPHABPOK) {
+        this.EXPHABPOK = EXPHABPOK;
+    }
+
+    
+    public double getPESPOK() {
+        return PESPOK;
+    }
+
+    public void setPESPOK(double PESPOK) {
+        this.PESPOK = PESPOK;
+    }
+
+    public String getIMGFROPOK() {
+        return IMGFROPOK;
+    }
+
+    public void setIMGFROPOK(String IMGFROPOK) {
+        this.IMGFROPOK = IMGFROPOK;
+    }
+
+    public String getIMGATRPOK() {
+        return IMGATRPOK;
+    }
+
+    public void setIMGATRPOK(String IMGATRPOK) {
+        this.IMGATRPOK = IMGATRPOK;
+    }
+
+    public Habilidades getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(Habilidades habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public String getEXPHABPOKString() {
+        return EXPHABPOKString;
+    }
+
+    public void setEXPHABPOKString(String EXPHABPOKString) {
+        this.EXPHABPOKString = EXPHABPOKString;
+    }
+
+
+    public String getPESPOKString() {
+        return PESPOKString;
+    }
+
+    public void setPESPOKString(String PESPOKString) {
+        this.PESPOKString = PESPOKString;
+    }
+
+    public double getALTPOK() {
+        return ALTPOK;
+    }
+
+    public void setALTPOK(double ALTPOK) {
+        this.ALTPOK = ALTPOK;
+    }
+
+    public String getALTPOKString() {
+        return ALTPOKString;
+    }
+
+    public void setALTPOKString(String ALTPOKString) {
+        this.ALTPOKString = ALTPOKString;
+    }
+
+   
+
+    
+
+
+}
